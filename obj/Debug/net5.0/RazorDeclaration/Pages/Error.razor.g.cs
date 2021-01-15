22a91eee43da1885790476f28e69a0899cb6e945
mserey <marcoserey22@gmail.com>
2021-01-15 16:06:35 -0300

    Se corrije error de dependencia

--- obj/Debug/net5.0/RazorDeclaration/Pages/Error.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Error.razor.g.cs
@@ -13,84 +13,84 @@ namespace Elsa.Samples.UserRegistration.Web.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Kibernum\Downloads\elsa-samples-master\elsa-samples-master\src\Elsa.Samples.UserRegistration\src\Elsa.Samples.UserRegistration.Web\_Imports.razor"
+#line 1 "C:\Trabajo\Kibernum\ISP Motor Procesos\Repo\POCELSA\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Kibernum\Downloads\elsa-samples-master\elsa-samples-master\src\Elsa.Samples.UserRegistration\src\Elsa.Samples.UserRegistration.Web\_Imports.razor"
+#line 2 "C:\Trabajo\Kibernum\ISP Motor Procesos\Repo\POCELSA\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Kibernum\Downloads\elsa-samples-master\elsa-samples-master\src\Elsa.Samples.UserRegistration\src\Elsa.Samples.UserRegistration.Web\_Imports.razor"
+#line 3 "C:\Trabajo\Kibernum\ISP Motor Procesos\Repo\POCELSA\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Kibernum\Downloads\elsa-samples-master\elsa-samples-master\src\Elsa.Samples.UserRegistration\src\Elsa.Samples.UserRegistration.Web\_Imports.razor"
+#line 4 "C:\Trabajo\Kibernum\ISP Motor Procesos\Repo\POCELSA\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Kibernum\Downloads\elsa-samples-master\elsa-samples-master\src\Elsa.Samples.UserRegistration\src\Elsa.Samples.UserRegistration.Web\_Imports.razor"
+#line 5 "C:\Trabajo\Kibernum\ISP Motor Procesos\Repo\POCELSA\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Kibernum\Downloads\elsa-samples-master\elsa-samples-master\src\Elsa.Samples.UserRegistration\src\Elsa.Samples.UserRegistration.Web\_Imports.razor"
+#line 6 "C:\Trabajo\Kibernum\ISP Motor Procesos\Repo\POCELSA\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Kibernum\Downloads\elsa-samples-master\elsa-samples-master\src\Elsa.Samples.UserRegistration\src\Elsa.Samples.UserRegistration.Web\_Imports.razor"
+#line 7 "C:\Trabajo\Kibernum\ISP Motor Procesos\Repo\POCELSA\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Kibernum\Downloads\elsa-samples-master\elsa-samples-master\src\Elsa.Samples.UserRegistration\src\Elsa.Samples.UserRegistration.Web\_Imports.razor"
+#line 8 "C:\Trabajo\Kibernum\ISP Motor Procesos\Repo\POCELSA\_Imports.razor"
 using Elsa.Samples.UserRegistration.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Kibernum\Downloads\elsa-samples-master\elsa-samples-master\src\Elsa.Samples.UserRegistration\src\Elsa.Samples.UserRegistration.Web\_Imports.razor"
+#line 9 "C:\Trabajo\Kibernum\ISP Motor Procesos\Repo\POCELSA\_Imports.razor"
 using Elsa.Samples.UserRegistration.Web.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Kibernum\Downloads\elsa-samples-master\elsa-samples-master\src\Elsa.Samples.UserRegistration\src\Elsa.Samples.UserRegistration.Web\_Imports.razor"
+#line 10 "C:\Trabajo\Kibernum\ISP Motor Procesos\Repo\POCELSA\_Imports.razor"
 using Elsa.Samples.UserRegistration.Web.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\Kibernum\Downloads\elsa-samples-master\elsa-samples-master\src\Elsa.Samples.UserRegistration\src\Elsa.Samples.UserRegistration.Web\_Imports.razor"
+#line 11 "C:\Trabajo\Kibernum\ISP Motor Procesos\Repo\POCELSA\_Imports.razor"
 using Elsa.Services;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\Kibernum\Downloads\elsa-samples-master\elsa-samples-master\src\Elsa.Samples.UserRegistration\src\Elsa.Samples.UserRegistration.Web\_Imports.razor"
+#line 12 "C:\Trabajo\Kibernum\ISP Motor Procesos\Repo\POCELSA\_Imports.razor"
 using Elsa.Services.Extensions;
 
 #line default
